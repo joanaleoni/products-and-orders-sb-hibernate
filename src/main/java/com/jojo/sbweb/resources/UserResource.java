56d3c3c7c1202c98c3f1af0a3b53087956d78d24
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-06 12:22:01 -0300

    User entity and resource

---
+++ src/main/java/com/jojo/sbweb/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.jojo.sbweb.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jojo.sbweb.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1, "Mary", "mary@gmail.com", "8888", "123");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
