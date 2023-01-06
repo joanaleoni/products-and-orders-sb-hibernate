1e2ab9ef5ccfefb2a94affa46d9b4ba09643e103
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-06 12:13:01 -0300

    Project created

---
+++ src/main/java/com/jojo/sbweb/SbwebApplication.java
@@ -0,0 +1,13 @@
+package com.jojo.sbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojo/sbweb/SbwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojo.sbweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
