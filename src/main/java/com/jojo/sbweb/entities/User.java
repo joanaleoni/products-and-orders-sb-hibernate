f08233eda4b456093371eb94d5f1f1cab6156e19
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-06 15:45:23 -0300

    One-to-many association

--- src/main/java/com/jojo/sbweb/entities/User.java
+++ src/main/java/com/jojo/sbweb/entities/User.java
@@ -1,11 +1,16 @@
 package com.jojo.sbweb.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,9 +25,12 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
-	public User() {
-	}
+	public User() {}
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
@@ -71,6 +79,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
